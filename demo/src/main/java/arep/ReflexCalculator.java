1ceb14261477cc95128e4064fb74503e884ee114
unknown <andres.martinez-v@labinfo.is.escuelaing.edu.co>
2023-09-14 12:54:12 -0500

    ¨¨parcial¨

---
+++ demo/src/main/java/arep/ReflexCalculator.java
@@ -0,0 +1,19 @@
+package arep;
+import java.lang.Math;
+
+public class ReflexCalculator {
+    public static double calculator(String metodo, double resolve){
+        Math.getMethod(metodo, resolve);
+        return resolve;
+    }
+//**Ejemplo de algoritmo:
+//QUICKSORT (arreglo A, inicio, fin)     
+//{  
+//  if (inicio < fin)     
+//  {  
+//     p = partition(A, inicio, fin)  
+//     QUICKSORT (A, inicio, p - 1)    
+//     QUICKSORT (A, p + 1, fin)    
+// }  
+//}   
+}
